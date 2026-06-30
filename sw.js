8fcf6a03fc1e6eb502c410f811549f315e45324b
kashu19921118-alt <kashu19921118@gmail.com>
2026-06-30 23:39:34 +0900

    feat: add service worker for PWA offline support

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'matsuri-v1';
+const PRECACHE = ['./', './index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(PRECACHE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(ks =>
+      Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit => {
+      const net = fetch(e.request).then(res => {
+        if (res && res.ok) {
+          caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        }
+        return res;
+      }).catch(() => {});
+      return hit || net;
+    })
+  );
+});
